528b29e436adf3e37c535e0e48dc21a33fdcef2f
Derek <legato_5@yahoo.com>
2020-01-15 18:36:20 -0800

    post request now working

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ server.listen(5000, () => {
 
 server.use(express.json());
 
-server.get('/api/users', (req, res) => {
+server.post("/api/users/", (req, res) => {
+    const userInfo = req.body;
+    if(userInfo.name && userInfo.bio) {
+        data.insert(userInfo)
+        .then(user => {
+            res.status(201).json({success:true, user})
+        })
+        .catch(err => {
+            res.status(500).json({success:false, errorMessage: "There was an error while saving the user to the database"})
+        })
+    }
+    else{
+        res.status(400).json({success:false, errorMessage: "Please provide name and bio for the user."})
+    }
+    
+})
+
+server.get("/api/users", (req, res) => {
     data.find()
         .then(users => {
             res.status(200).json(users);
