fab6595d52225fb72047c390f07d105ffc24c97c
Derek <legato_5@yahoo.com>
2020-01-15 18:22:15 -0800

    started server and made first get request

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // implement your API here
-hi
+
+const express = require('express')
+const data = require('./data/db.js');
+
+const server = express();
+
+server.listen(5000, () => {
+    console.log("*** listening on port 5000")
+});
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    data.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({success:false, err})
+        })
+})
