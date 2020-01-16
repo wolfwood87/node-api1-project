d35ff01ae9df6f1bce9aac2f77039ce35a72b520
Derek <legato_5@yahoo.com>
2020-01-15 20:40:31 -0800

    add button now working. All that's left is styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserList from './Components/UserList';
+import axios from 'axios';
 
 function App() {
+  const [newForm, setNewForm] = useState(false);
+  const [newUser, setNewUser] = useState({name: '', bio: ''})
+
+
+  const handleNewUser = e => {
+    e.preventDefault();
+    setNewForm(true);
+  }
+
+  const handleChange = e => {
+    e.preventDefault();
+    setNewUser({
+        ...newUser,
+        [e.target.name]: e.target.value
+    })
+  }
+  const submitNewUser = e => {
+    e.preventDefault();
+    axios
+        .post(`http://localhost:5000/api/users/`, newUser)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
   return (
     <div className="App">
+      {newForm && 
+            <div>
+                <input
+                    type='text'
+                    name="name"
+                    placeholder="Name"
+                    value={newUser.name}
+                    onChange={handleChange}
+                    />
+                <input
+                    type='text'
+                    name="bio"
+                    placeholder="Bio"
+                    value={newUser.bio}
+                    onChange={handleChange}
+                    />
+                <button onClick={submitNewUser}>Submit</button>
+            </div>
+            }
+      {!newForm && 
+        <button onClick={handleNewUser}>Add User</button>
+      }
       <UserList />
     </div>
   );
